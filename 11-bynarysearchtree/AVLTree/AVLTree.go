1e2feea1f82a53188c935cc2453b450d4da2fdbd
Bruno Gabriel <bruno.glm_@hotmail.com>
2024-12-10 19:58:42 -0300

    add content

--- 11-bynarysearchtree/AVLTree/AVLTree.go
+++ 11-bynarysearchtree/AVLTree/AVLTree.go
@@ -27,6 +27,41 @@ func NewTree[T constraints.Ordered](compareFn func(node1, node2 T) int) *AVLTree
 	}
 }
 
+func (t *AVLTree[T]) Insert(key T) {
+	t.Root = t.insertNode(t.Root, key)
+}
+
+func (t *AVLTree[T]) insertNode(node *bst.Node[T], key T) *bst.Node[T] {
+	if node == nil {
+		return bst.NewNode(key)
+	} else if t.CompareFn(key, node.Key) == -1 {
+		node.Left = t.insertNode(node.Left, key)
+	} else if t.CompareFn(key, node.Key) == 1 {
+		node.Right = t.insertNode(node.Right, key)
+	} else {
+		return node
+	}
+
+	balanceFactor := t.getBalance(node)
+	if balanceFactor == UNBALANCED_LEFT {
+		if t.CompareFn(key, node.Key) == -1 {
+			node = t.rotationLL(node)
+		} else {
+			return t.rotationLR(node)
+		}
+	}
+
+	if balanceFactor == UNBALANCED_RIGHT {
+		if t.CompareFn(key, node.Key) == 1 {
+			node = t.rotationRR(node)
+		} else {
+			return t.rotationRL(node)
+		}
+	}
+
+	return node
+}
+
 func (t *AVLTree[T]) getNodeHeight(node *bst.Node[T]) int {
 	if node == nil {
 		return -1
@@ -51,4 +86,28 @@ func (t *AVLTree[T]) getBalance(node *bst.Node[T]) BalanceFactor {
 	default:
 		return BALANCED
 	}
+}
+
+func (t *AVLTree[T]) rotationLL(node *bst.Node[T]) *bst.Node[T] {
+	tmp := node.Left
+	node.Left = tmp.Right
+	tmp.Right = node
+	return tmp
+}
+
+func (t *AVLTree[T]) rotationRR(node *bst.Node[T]) *bst.Node[T] {
+	tmp := node.Right
+	node.Right = tmp.Left
+	tmp.Left = node
+	return tmp
+}
+
+func (t *AVLTree[T]) rotationLR(node *bst.Node[T]) *bst.Node[T] {
+	node.Left = t.rotationRR(node.Left)
+	return t.rotationLL(node)
+}
+
+func (t *AVLTree[T]) rotationRL(node *bst.Node[T]) *bst.Node[T] {
+	node.Right = t.rotationLL(node.Right)
+	return t.rotationRR(node)
 }
